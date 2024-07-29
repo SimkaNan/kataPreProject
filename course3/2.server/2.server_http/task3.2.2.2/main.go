bdaeff1cabacf89bc84ad0915a0d75e115f0ce33
Jillian Cobblepot <jilliaco@mi-i6.msk.21-school.ru>
2024-07-29 18:26:03 +0300

    Fix

--- course3/2.server/2.server_http/task3.2.2.2/main.go
+++ course3/2.server/2.server_http/task3.2.2.2/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello from Docker container!</h1>")
-	})
-
-	err := http.ListenAndServe("localhost:8080", nil)
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file")
 	}
+
+	port := os.Getenv("PORT")
+
+	log.Printf("Сервер запущен на порту %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
+
+// func main() {
+// 	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Fprintf(w, "<h1>Hello from Docker container!</h1>")
+// 	})
+
+// 	err := http.ListenAndServe("localhost:8080", nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
