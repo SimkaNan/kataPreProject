9bfdd6638966a8a57b0f6203cbd196401513eb95
simkanani <fnfnfnfjc@gmail.com>
2024-07-30 16:55:15 +0300

    Fix

--- course3/2.server/2.server_http/task3.2.2.2/main.go
+++ course3/2.server/2.server_http/task3.2.2.2/main.go
@@ -20,21 +20,21 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// )
-
-// func main() {
-// 	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintf(w, "<h1>Hello from Docker container!</h1>")
-// 	})
-
-// 	err := http.ListenAndServe("localhost:8080", nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+//package main
+//
+//import (
+//	"fmt"
+//	"log"
+//	"net/http"
+//)
+//
+//func main() {
+//	http.HandleFunc("/docker", func(w http.ResponseWriter, r *http.Request) {
+//		fmt.Fprintf(w, "<h1>Hello from Docker container!</h1>")
+//	})
+//
+//	err := http.ListenAndServe("localhost:8080", nil)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//}
